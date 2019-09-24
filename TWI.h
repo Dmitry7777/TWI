95f2b669730d15c84334cc21eeca06ff04998e90
Dmitry7777 <list-dmitrij@mail.ru>
2019-09-24 22:49:20 +0600

    Update TWI.h

--- TWI.h
+++ TWI.h
@@ -3,6 +3,81 @@
 
 class TWI{
   public:
+  TWI(){
+    
+  }
+  void begin(){
+    
+  }
+  void begin(uint8_t){
+    
+  }
+  void begin(int){
+    
+  }
+ void setClock(){
+    
+  }
+  void beginTransmission(uint8_t){
+    
+  }
+  void beginTransmission(int){
+    
+  }
+  uint8_t endTransmission(void){
+    
+  }
+  uint8_t endTransmission(uint8_t){
+    
+  }
+  uint8_t requestFrom(uint8_t, uint8_t){
+    
+  }
+  uint8_t requestFrom(uint8_t, uint8_t, uint8_t){
+    
+  }
+  uint8_t requestFrom(uint8_t, uint8_t, uint32_t, uint8_t, uint8_t){
+    
+  }
+  uint8_t requestFrom(int, int){
+    
+  }
+   uint8_t requestFrom(int, int, int){
+    
+  }
+  virtual size_t write(uint8_t){
+    
+  }
+  virtual size_t write(const uint8_t *, size_t){
+    
+  }
+  virtual int available(void){
+    
+  }
+  virtual int read(void){
+    
+  }
+  virtual int peek(void){
+    
+  }
+  virtual void flush(void){
+    
+  }
+  virtual void onReceive(void(*)(int)){
+  
+ }
+ virtual void onRequest(void(*)(void)){
+  
+ }
+  void end(){
+    
+  }
+  inline size_t write(unsigned long n) { return write((uint8_t)n); }
+  inline size_t write(long n) { return write((uint8_t)n); }
+  inline size_t write(unsigned int n) { return write((uint8_t)n); }
+  inline size_t write(int n) { return write((uint8_t)n); }
+//    using Print::write;
+
   private:
   protected:
 };
